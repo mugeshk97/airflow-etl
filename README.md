006410704a84ac62171cc2763573ddd0e63d18d0
Mugesh <mugeshk1171@gmail.com>
2021-09-27 20:36:42 +0530

    dag modified

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # airflow-etl
+
+docker compose command
+to start the airflow server
+```
+docker-compose up -d
+```
+to stop the server
+```
+docker-compose down
+```
--- dags/main_dag.py
+++ dags/main_dag.py
@@ -1,14 +1,26 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.mysql_operator import MySqlOperator
+
+
+
+def print_hello():
+    for i in range(1, 10):
+        print("Hello world")
 
 args = {
-    'owner': 'airflow',
+    'owner': 'mugesh',
     'start_date': datetime(2021, 9, 8),
     'retries':1,
     'retry_delay':timedelta(seconds=5)
     }
 
-dag = DAG('dag1', default_args=args, schedule_interval='@daily', catchup=False)
+with DAG('ETL', default_args=args, schedule_interval='@daily', catchup=False) as dag:
 
-task1 = BashOperator(task_id='task1', bash_command='echo "Hello World"', dag=dag)
+    task1 = BashOperator(task_id='hello', bash_command='echo "Hello World"')
+
+    task2 = PythonOperator(task_id='hello_python', python_callable=print_hello)
+
+    task1 >> task2
